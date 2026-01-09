452fc5c8b6621ddf94d6d4cb07c58f2c7b586e76
MyProjects-web <forpower333777@gmail.com>
2026-01-09 11:42:11 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+
+
+import 'ui/get_started.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Weather App',
+      home: GetStarted(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
